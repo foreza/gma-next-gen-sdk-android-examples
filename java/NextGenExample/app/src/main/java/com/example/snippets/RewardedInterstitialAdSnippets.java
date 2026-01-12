e1a192b0483ae90371e993d01d6f3771ba58e6d1
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-01-12 20:42:13 +0000

    No public description
    PiperOrigin-RevId: 855343512

---
+++ java/NextGenExample/app/src/main/java/com/example/snippets/RewardedInterstitialAdSnippets.java
@@ -0,0 +1,40 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example.snippets;
+
+import com.google.android.libraries.ads.mobile.sdk.common.AdRequest;
+import com.google.android.libraries.ads.mobile.sdk.common.PreloadConfiguration;
+import com.google.android.libraries.ads.mobile.sdk.rewardedinterstitial.RewardedInterstitialAd;
+import com.google.android.libraries.ads.mobile.sdk.rewardedinterstitial.RewardedInterstitialAdPreloader;
+
+/** Java code snippets for the developer guide. */
+public final class RewardedInterstitialAdSnippets {
+
+  // [START start_preload]
+  private void startPreloading(String adUnitId) {
+    AdRequest adRequest = new AdRequest.Builder(adUnitId).build();
+    PreloadConfiguration preloadConfig = new PreloadConfiguration(adRequest);
+    RewardedInterstitialAdPreloader.start(adUnitId, preloadConfig);
+  }
+
+  // [END start_preload]
+
+  private void pollAd(String adUnitId) {
+    // [START poll_ad]
+    // Polling returns the next available ad and loads another ad in the background.
+    final RewardedInterstitialAd ad = RewardedInterstitialAdPreloader.pollAd(adUnitId);
+    // [END poll_ad]
+  }
+}
---
+++ kotlin/NextGenExample/app/src/main/java/com/example/snippets/RewardedInterstitialAdSnippets.kt
@@ -0,0 +1,39 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example.snippets
+
+import com.google.android.libraries.ads.mobile.sdk.common.AdRequest
+import com.google.android.libraries.ads.mobile.sdk.common.PreloadConfiguration
+import com.google.android.libraries.ads.mobile.sdk.rewardedinterstitial.RewardedInterstitialAdPreloader
+
+/** Kotlin code snippets for the developer guide. */
+private class RewardedInterstitialAdSnippets {
+
+  // [START start_preload]
+  private fun startPreloading(adUnitId: String) {
+    val adRequest = AdRequest.Builder(adUnitId).build()
+    val preloadConfig = PreloadConfiguration(adRequest)
+    RewardedInterstitialAdPreloader.start(adUnitId, preloadConfig)
+  }
+
+  // [END start_preload]
+
+  private fun pollAd(adUnitId: String) {
+    // [START poll_ad]
+    // Polling returns the next available ad and loads another ad in the background.
+    val ad = RewardedInterstitialAdPreloader.pollAd(adUnitId)
+    // [END poll_ad]
+  }
+}
